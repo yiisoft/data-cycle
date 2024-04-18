85640401fcbacfae16aeef13fd1c630ea732a48b
Alexey Rogachev <arogachev90@gmail.com>
2024-04-18 19:37:53 +0500

    Test again

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-if (getenv('ENVIRONMENT', local_only: true) !== 'production') {
+if (getenv('ENVIRONMENT', local_only: true) !== 'ci') {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
     $dotenv->load();
 }
