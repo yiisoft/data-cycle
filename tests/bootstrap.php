8081cb462620e9bdad2ea5e1bc01b4f00d8f6968
Alexey Rogachev <arogachev90@gmail.com>
2024-04-18 19:04:07 +0500

    Test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,8 @@
 
 declare(strict_types=1);
 
+var_dump(getenv('COMPOSER_CACHE_DIR', local_only: true));
+exit();
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env.local');
 $dotenv->load();
