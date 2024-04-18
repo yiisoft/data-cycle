fef6d2867d9fb2be512d933d5e3518619df534cf
Alexey Rogachev <arogachev90@gmail.com>
2024-04-18 19:21:40 +0500

    Test 4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-var_dump(getenv('ENVIRONMENT', local_only: true));
-var_dump(getenv('CYCLE_MYSQL_DATABASE', local_only: true));
-exit;
-
 if (getenv('ENVIRONMENT', local_only: true) !== 'production') {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
     $dotenv->load();
