32c8618569afaeb8c4caa549b6f7d6661e0f4a89
Alexey Rogachev <arogachev90@gmail.com>
2024-04-18 14:33:49 +0500

    Debug MySQL

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+var_dump($_ENV);
 //if ($_ENV['ENVIRONMENT'] ?? null !== 'production') {
 //    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 //    $dotenv->load();
