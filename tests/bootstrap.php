77e398ca7e3a6d459805e9b9d5c3b43656a944cd
Alexey Rogachev <arogachev90@gmail.com>
2024-04-18 18:01:41 +0500

    Revert removed workflows

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-//if ($_ENV['ENVIRONMENT'] ?? null !== 'production') {
-//    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-//    $dotenv->load();
-//}
+if ($_ENV['ENVIRONMENT'] ?? null !== 'production') {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
