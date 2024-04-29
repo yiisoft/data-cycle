8cb6e025e5d197aba2e8c391ae3a689427c75a80
Alexey Rogachev <arogachev90@gmail.com>
2024-04-29 21:34:01 +0500

    Fix wrong filter usage

--- tests/Feature/Base/Reader/FilterHandler/ILikeHandlerTest.php
+++ tests/Feature/Base/Reader/FilterHandler/ILikeHandlerTest.php
@@ -6,13 +6,13 @@ namespace Yiisoft\Data\Cycle\Tests\Feature\Base\Reader\FilterHandler;
 
 use Yiisoft\Data\Cycle\Reader\EntityReader;
 use Yiisoft\Data\Cycle\Tests\Feature\BaseData;
-use Yiisoft\Data\Reader\Filter\Like;
+use Yiisoft\Data\Reader\Filter\ILike;
 
 abstract class ILikeHandlerTest extends BaseData
 {
     public function testILikeHandler(): void
     {
-        $reader = (new EntityReader($this->select('user')))->withFilter(new Like('email', 'SEED@%'));
+        $reader = (new EntityReader($this->select('user')))->withFilter(new ILike('email', 'SEED@%'));
         $this->assertFixtures([2], $reader->read());
     }
 }
