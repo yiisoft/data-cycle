fd65e8b7af466753b3c9b3e5b0a7ea7f7b98421a
Alexey Rogachev <arogachev90@gmail.com>
2024-04-30 21:12:58 +0500

    More test cases

--- tests/Feature/Base/Reader/FilterHandler/ILikeHandlerTest.php
+++ tests/Feature/Base/Reader/FilterHandler/ILikeHandlerTest.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace Yiisoft\Data\Cycle\Tests\Feature\Base\Reader\FilterHandler;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use Yiisoft\Data\Cycle\Reader\EntityReader;
 use Yiisoft\Data\Cycle\Tests\Feature\BaseData;
 use Yiisoft\Data\Reader\Filter\ILike;
 
 abstract class ILikeHandlerTest extends BaseData
 {
-    public function testILikeHandler(): void
+    public static function dataBase(): array
     {
-        $reader = (new EntityReader($this->select('user')))->withFilter(new ILike('email', 'SEED@%'));
-        $this->assertFixtures([2], $reader->read());
+        return [
+            'case matches' => ['email', 'seed@%', [2]],
+            'case does not match' => ['email', 'SEED@%', [2]],
+        ];
+    }
+
+    #[DataProvider('dataBase')]
+    public function testBase(string $field, string $value, array $expectedFixtureIndexes): void
+    {
+        $reader = (new EntityReader($this->select('user')))->withFilter(new ILike($field, $value));
+        $this->assertFixtures($expectedFixtureIndexes, $reader->read());
     }
 }
--- tests/Feature/Base/Reader/FilterHandler/LikeHandlerTest.php
+++ tests/Feature/Base/Reader/FilterHandler/LikeHandlerTest.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace Yiisoft\Data\Cycle\Tests\Feature\Base\Reader\FilterHandler;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use Yiisoft\Data\Cycle\Reader\EntityReader;
 use Yiisoft\Data\Cycle\Tests\Feature\BaseData;
 use Yiisoft\Data\Reader\Filter\Like;
 
 abstract class LikeHandlerTest extends BaseData
 {
-    public function testLikeHandler(): void
+    public static function dataBase(): array
     {
-        $reader = (new EntityReader($this->select('user')))->withFilter(new Like('email', 'seed@%'));
-        $this->assertFixtures([2], $reader->read());
+        return [
+            'case matches' => ['email', 'seed@%', [2]],
+            'case does not match' => ['email', 'SEED@%', []],
+        ];
+    }
+
+    #[DataProvider('dataBase')]
+    public function testBase(string $field, string $value, array $expectedFixtureIndexes): void
+    {
+        $reader = (new EntityReader($this->select('user')))->withFilter(new Like($field, $value));
+        $this->assertFixtures($expectedFixtureIndexes, $reader->read());
     }
 }
