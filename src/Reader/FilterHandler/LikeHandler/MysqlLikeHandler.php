63966873a2f0b664d0c86bd9a14e8fba9fa4489d
Alexey Rogachev <arogachev90@gmail.com>
2024-10-03 20:44:12 +0500

    WIP

--- src/Reader/FilterHandler/LikeHandler/MysqlLikeHandler.php
+++ src/Reader/FilterHandler/LikeHandler/MysqlLikeHandler.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace Yiisoft\Data\Cycle\Reader\FilterHandler\LikeHandler;
 
+use Yiisoft\Data\Cycle\Exception\UnexpectedFilterException;
+use Yiisoft\Data\Reader\Filter\Like;
+use Yiisoft\Data\Reader\FilterInterface;
+
 final class MysqlLikeHandler extends BaseLikeHandler
 {
+    public function getAsWhereArguments(FilterInterface $filter, array $handlers): array
+    {
+        if (!$filter instanceof Like) {
+            throw new UnexpectedFilterException(Like::class, $filter::class);
+        }
+
+        if ($filter->isCaseSensitive() !== true) {
+            return [$filter->getField(), 'like', '%' . $filter->getValue() . '%'];
+        }
+
+        return [$filter->getField(), 'like binary', '%' . $filter->getValue() . '%'];
+    }
 }
