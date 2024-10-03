dc20aab389d2059d840ef8299d6c72b16a756f88
Alexey Rogachev <arogachev90@gmail.com>
2024-10-03 16:12:28 +0500

    Fix 3

--- tests/Feature/Base/Reader/ReaderWithFilter/BaseReaderWithLikeTestCase.php
+++ tests/Feature/Base/Reader/ReaderWithFilter/BaseReaderWithLikeTestCase.php
@@ -14,6 +14,7 @@ abstract class BaseReaderWithLikeTestCase extends \Yiisoft\Data\Tests\Common\Rea
     {
         $data = parent::dataWithReader();
         unset(
+            $data['search: contains, different case, case sensitive: false'],
             $data['search: contains, different case, case sensitive: true'],
             $data['wildcard is not supported'],
         );
