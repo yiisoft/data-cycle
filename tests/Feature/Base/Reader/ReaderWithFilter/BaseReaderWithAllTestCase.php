c04849a4e0bc59e9e6be6763dd56ebbba7d8714b
Ross Addison <rossaddison@users.noreply.github.com>
2025-08-18 10:42:05 +0100

    Test All replaced with AndX

--- tests/Feature/Base/Reader/ReaderWithFilter/BaseReaderWithAllTestCase.php
+++ tests/Feature/Base/Reader/ReaderWithFilter/BaseReaderWithAllTestCase.php
@@ -8,10 +8,10 @@ use Yiisoft\Data\Cycle\Exception\NotSupportedFilterException;
 use Yiisoft\Data\Cycle\Reader\EntityReader;
 use Yiisoft\Data\Cycle\Tests\Feature\DataTrait;
 use Yiisoft\Data\Cycle\Tests\Support\NotSupportedFilter;
-use Yiisoft\Data\Reader\Filter\All;
+use Yiisoft\Data\Reader\Filter\AndX;
 use Yiisoft\Data\Reader\Filter\Equals;
 
-abstract class BaseReaderWithAllTestCase extends \Yiisoft\Data\Tests\Common\Reader\ReaderWithFilter\BaseReaderWithAllTestCase
+abstract class BaseReaderWithAndXTestCase extends \Yiisoft\Data\Tests\Common\Reader\ReaderWithFilter\BaseReaderWithAllTestCase
 {
     use DataTrait;
 
@@ -22,7 +22,7 @@ abstract class BaseReaderWithAllTestCase extends \Yiisoft\Data\Tests\Common\Read
         $this->expectException(NotSupportedFilterException::class);
         $this->expectExceptionMessage(sprintf('Filter "%s" is not supported.', NotSupportedFilter::class));
         $reader->withFilter(
-            new All(new Equals('balance', '100.0'), new NotSupportedFilter(), new Equals('email', 'seed@beat')),
+            new AndX(new Equals('balance', '100.0'), new NotSupportedFilter(), new Equals('email', 'seed@beat')),
         );
     }
 }
--- tests/Feature/Base/Reader/ReaderWithFilter/BaseReaderWithOrXTestCase.php
+++ tests/Feature/Base/Reader/ReaderWithFilter/BaseReaderWithOrXTestCase.php
@@ -8,10 +8,10 @@ use Yiisoft\Data\Cycle\Exception\NotSupportedFilterException;
 use Yiisoft\Data\Cycle\Reader\EntityReader;
 use Yiisoft\Data\Cycle\Tests\Feature\DataTrait;
 use Yiisoft\Data\Cycle\Tests\Support\NotSupportedFilter;
-use Yiisoft\Data\Reader\Filter\Any;
+use Yiisoft\Data\Reader\Filter\OrX;
 use Yiisoft\Data\Reader\Filter\Equals;
 
-abstract class BaseReaderWithAnyTestCase extends \Yiisoft\Data\Tests\Common\Reader\ReaderWithFilter\BaseReaderWithAnyTestCase
+abstract class BaseReaderWithOrXTestCase extends \Yiisoft\Data\Tests\Common\Reader\ReaderWithFilter\BaseReaderWithAnyTestCase
 {
     use DataTrait;
 
@@ -21,6 +21,6 @@ abstract class BaseReaderWithAnyTestCase extends \Yiisoft\Data\Tests\Common\Read
 
         $this->expectException(NotSupportedFilterException::class);
         $this->expectExceptionMessage(sprintf('Filter "%s" is not supported.', NotSupportedFilter::class));
-        $reader->withFilter(new Any(new Equals('number', 2), new NotSupportedFilter(), new Equals('number', 3)));
+        $reader->withFilter(new OrX(new Equals('number', 2), new NotSupportedFilter(), new Equals('number', 3)));
     }
 }
