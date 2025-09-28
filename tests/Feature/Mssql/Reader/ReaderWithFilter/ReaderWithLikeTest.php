5b260bb3a61dfb8e9a156b4fdf3265399b3ad597
Mahmud <magamedov2010@gmail.com>
2025-09-28 04:24:14 +0300

    Overriding test method to assert exception an unsupported case sensitivity

--- tests/Feature/Mssql/Reader/ReaderWithFilter/ReaderWithLikeTest.php
+++ tests/Feature/Mssql/Reader/ReaderWithFilter/ReaderWithLikeTest.php
@@ -4,9 +4,26 @@ declare(strict_types=1);
 
 namespace Yiisoft\Data\Cycle\Tests\Feature\Mssql\Reader\ReaderWithFilter;
 
+use PHPUnit\Framework\Attributes\DataProvider;
+use Yiisoft\Data\Cycle\Exception\NotSupportedFilterOptionException;
 use Yiisoft\Data\Cycle\Tests\Feature\Base\Reader\ReaderWithFilter\BaseReaderWithLikeTestCase;
 
 final class ReaderWithLikeTest extends BaseReaderWithLikeTestCase
 {
     public static $DRIVER = 'mssql';
+
+    #[DataProvider('dataWithReader')]
+    public function testWithReader(
+        string $field,
+        mixed $value,
+        bool|null $caseSensitive,
+        array $expectedFixtureIndexes,
+    ): void {
+        if ($caseSensitive === true) {
+            $this->expectException(NotSupportedFilterOptionException::class);
+            $this->expectExceptionMessage('$caseSensitive option is not supported when using SQLServer driver.');
+        }
+
+        parent::testWithReader($field, $value, $caseSensitive, $expectedFixtureIndexes);
+    }
 }
